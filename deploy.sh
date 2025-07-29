12ccc8e5949d273e506608c5a643759dffb30659
pwnpanda <robinnr1@gmail.com>
2025-07-29 20:37:03 +0200

    Working

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,31 @@ git submodule foreach git pull origin main
 # Step 2: Update the homepage in package.json
 echo "Updating homepage in package.json..."
 jq '.homepage = "/reqbin/"' interactsh-web/package.json > tmp.json && mv tmp.json interactsh-web/package.json
-rm tmp.json
+
+echo '# Base build stage
+FROM node:16 as builder
+
+WORKDIR /app
+COPY package.json yarn.lock ./
+COPY . .
+
+RUN yarn install && yarn build
+
+# Serve build with a static file server
+FROM node:16 as runner
+
+WORKDIR /app
+RUN yarn global add serve
+
+COPY --from=builder /app/build ./build
+
+EXPOSE 3000
+CMD ["serve", "-s", "build", "-l", "3000"]
+'>interactsh-web/Dockerfile
 
 # Step 3: Deploy using docker-compose
 echo "Deploying using docker-compose..."
-docker-compose up -d
+docker-compose up -d --build
 
 echo "Deployment complete."
 
