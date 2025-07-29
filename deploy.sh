9f0a816e36ba739c3439df2cbe6bb9e98cbd011a
pwnpanda <robinnr1@gmail.com>
2025-07-29 13:06:56 +0200

    Test

--- deploy.sh
+++ deploy.sh
@@ -30,8 +30,7 @@ fi
 if ! command_exists "asdf"; then
     echo "asdf is not installed. Installing using Go..."
     go install github.com/asdf-vm/asdf.git@latest
-    echo 'PATH="$HOME/.asdf/shims:$PATH"' >> ~/.zshrc
-    source ~/.zshrc
+    export PATH="$HOME/.asdf/shims:$PATH"
 else
     echo "asdf is already installed."
 fi
@@ -49,20 +48,12 @@ fi
 # Check for other required commands and install if missing
 echo "Checking for other required commands..."
 
-REQUIRED_COMMANDS=("jq" "git" "docker-compose")
+REQUIRED_COMMANDS=("jq" "git")
 
 for cmd in "${REQUIRED_COMMANDS[@]}"; do
     install_if_missing "$cmd"
 done
 
-# Check for other required commands and install if missing
-echo "Checking for other required commands..."
-
-REQUIRED_COMMANDS=("jq" "git" "docker-compose")
-
-for cmd in "${REQUIRED_COMMANDS[@]}"; do
-    install_if_missing "$cmd"
-done
 
 # Step 1: Pull the latest changes from the main repo and submodules
 echo "Pulling latest changes from the main repository and submodules..."
@@ -72,7 +63,8 @@ git submodule foreach git pull origin master
 
 # Step 2: Update the homepage in package.json
 echo "Updating homepage in package.json..."
-jq '.homepage = "/reqbin/"' package.json > tmp.json && mv tmp.json package.json
+jq '.homepage = "/reqbin/"' interact-sh/package.json > tmp.json && mv tmp.json interact-sh/package.json
+rm tmp.json
 
 # Step 3: Install required dependencies
 echo "Installing required dependencies..."
